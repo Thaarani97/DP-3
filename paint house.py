27dbbe95d223c945054ec498344e03cb725e513b
Thaarani <thaaranis13@gmail.com>
2022-07-29 01:02:30 -0400

    completed

---
+++ paint house.py
@@ -0,0 +1,11 @@
+#TC: O(n)
+#SC: O(1)
+
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        for i in range(1,len(costs)):
+            costs[i][0] = min(costs[i-1][1],costs[i-1][2])+costs[i][0]
+            costs[i][1] = min(costs[i-1][0],costs[i-1][2])+costs[i][1]
+            costs[i][2] = min(costs[i-1][0],costs[i-1][1])+costs[i][2]
+        
+        return min(costs[-1])
